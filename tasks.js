595cbb01361cb69f85a39fa23706652b4802b765
Georgete Bueno <geobueno@gmail.com>
2018-08-14 14:32:47 -0300

    Nomes

--- tasks.js
+++ tasks.js
@@ -2,68 +2,83 @@ var database = firebase.database();
 var userID = window.location.search.match(/\?id=(.*)/)[1];
 
 $(document).ready(function() {
-   getTasksFromDB();
-   $(".add-tasks").click(addTasksClick);
+    getTasksFromDB();
+    $(".add-tasks").click(addTasksClick);
 
 });
 
 function addTasksClick(event) {
-   event.preventDefault();
-   var newTask = $(".tasks-input").val();
-   var taskFromDB = addTaskToDB(newTask);
-   crudListItem(newTask, taskFromDB.key)
+    event.preventDefault();
+    var newTask = $(".tasks-input").val();
+    var taskFromDB = addTaskToDB(newTask);
+    crudListItem(newTask, taskFromDB.key)
 }
 
 function addTaskToDB(text) {
-   return database.ref("tasks/" + userID).push({
-       text: text
-   });
+    return database.ref("tasks/" + userID).push({
+        text: text
+    });
 }
 
 function getTasksFromDB() {
-   database.ref("tasks/" + userID).once('value')
-       .then(function(snapshot) {
-           snapshot.forEach(function(childSnapshot) {
-               var childKey = childSnapshot.key;
-               var childData = childSnapshot.val();
-               crudListItem(childData.text, childKey)
-           });
-       });
+    database.ref("tasks/" + userID).once('value')
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var childKey = childSnapshot.key;
+                var childData = childSnapshot.val();
+                crudListItem(childData.text, childKey)
+            });
+        });
+    database.ref("users/" + userID).once('value')
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var childKey = childSnapshot.key;
+                var childData = childSnapshot.val();
+                userListItem(childData.Name, childKey)
+            });
+        });
+}
+
+function userListItem(name, key) {
+    $(".users-list").append(`
+   <li>
+     <span>${name}</span>
+     <button class="follow" data-task-id=${key}>Follow</button>
+     <button class="unfollow" data-task-id=${key}>Unfollow</button>
+   </li>`);
 }
 
 function crudListItem(text, key) {
-   $(".tasks-list").append(`
+    $(".tasks-list").append(`
    <li>
      <span>${text}</span>
      <button class="delete" data-task-id=${key}>Deletar</button>
      <button class="edit" data-task-id=${key}>Editar</button>
    </li>`);
 
-   $(`button.delete[data-task-id="${key}"]`).click(function() {
-       database.ref("tasks/" + userID + "/" + key).remove();
-       $(this).parent().remove();
-   });
+    $(`button.delete[data-task-id="${key}"]`).click(function() {
+        database.ref("tasks/" + userID + "/" + key).remove();
+        $(this).parent().remove();
+    });
 
-   $(`button.edit[data-task-id="${key}"]`).click(function() {
-       database.ref("tasks/" + userID + "/" + key).remove();
-       $(this).parent().remove();
-       $(".tasks-list").append(`
+    $(`button.edit[data-task-id="${key}"]`).click(function() {
+        database.ref("tasks/" + userID + "/" + key).remove();
+        $(this).parent().remove();
+        $(".tasks-list").append(`
        <li>
        <textarea class="tasks-update">${text}</textarea>
          <button class="update-tasks">Update</button>
        </li>`);
-       $(".update-tasks").click(updateTasksClick);
-
-
-   });
+        $(".update-tasks").click(updateTasksClick);
+    });
 }
 
 function updateTasksClick(event) {
-   event.preventDefault();
-   var newTask = $(".tasks-update").val();
-   var taskFromDB = addTaskToDB(newTask);
-   crudListItem(newTask, taskFromDB.key);
-   $(".tasks-update").parent().remove();
-   $(".update-tasks").parent().remove();
+    event.preventDefault();
+    var newTask = $(".tasks-update").val();
+    var taskFromDB = addTaskToDB(newTask);
+    crudListItem(newTask, taskFromDB.key);
+    $(".tasks-update").parent().remove();
+    $(".update-tasks").parent().remove();
 
 }
